9f247dc2691a51ae09e0cee77660687be853d2d0
ddysto <107191027+ddysto@users.noreply.github.com>
2022-06-10 00:12:39 +0200

    !

--- WorkerService.js
+++ WorkerService.js
@@ -1,1 +1,27 @@
-WorkerService
+//self.importScripts('./service-worker-assets.js');
+self.addEventListener('install', event => event.waitUntil(onInstall(event)));
+self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
+self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+
+const cacheNamePrefix = 'offline-cache-';
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+//const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
+//const offlineAssetsExclude = [ /^service-worker\.js$/ ];
+
+async function onInstall(event) {
+    console.info('Service worker: Install');
+
+    }
+
+async function onActivate(event) {
+    console.info('Service worker: Activate');
+
+   
+
+async function onFetch(event) {
+    let cachedResponse = null;
+
+ 
+
+    return cachedResponse || fetch(event.request);
+}
